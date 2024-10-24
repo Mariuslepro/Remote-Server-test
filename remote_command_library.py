7e412c5c4c1360bc69e90a0102a1e0d4c8c5be77
Mariuslepro <127448511+Mariuslepro@users.noreply.github.com>
2024-10-24 01:54:56 -0500

    Update remote_command_library.py

--- remote_command_library.py
+++ remote_command_library.py
@@ -1,10 +1,11 @@
 import paramiko
 
+
 class remote_command_library:
 
     # takes server's parameters, to connect To the Server, Execute the Command and return
     # result if needed in a string
-    def connecToServerExecuteCommand(self, hostname, port, username, password, command):
+    def connecToServerExecuteCommand(self, hostname, port, username, password, command) -> str:
         # Initialize the SSH client
         ssh = paramiko.SSHClient()
 
@@ -30,66 +31,45 @@ class remote_command_library:
 
     # Working on windows pc
 
-    #Return the Network Interface Information on MWindows
-    def get_network_info2(self, hostname, port, username, password):
+    # Reboot a Windows server
+    def reboot_server_remotely(self, hostname, port, username, password):
+        command = r"shutdown /r /t 0"
+        remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
+
+    # Return the Network Interface Information on MWindows
+    def get_network_info2(self, hostname, port, username, password) -> str:
         command = r"ipconfig"
         result = remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
         return result
 
+    def get_pcie_info2(self, hostname, port, username, password) -> str:
 
-    def get_pcie_info2(self, hostname, port, username, password):
-
-        #pcie numbers
+        #Numbers of pcie's devvices
         command2 = 'wmic path win32_pnpentity where "DeviceID like \'%PCI%\'" get Name,DeviceID | find /c /v ""'
-        result2 = "\n the number of PCIE is: " + remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command2)
-        
-        # Get pcie info
+        result2 = "\n the number of PCIE is: " + remote_command_library.connecToServerExecuteCommand(self, hostname,
+                                                                                                     port, username,
+                                                                                                     password, command2)
+
+        # Get pcie,s devices info
         #command = "wmic path win32_pnpentity get name, deviceid | findstr PCI "
         command = 'wmic path win32_pnpentity where "DeviceID like \'%PCI%\'" get Name,DeviceID'
-        result = "\n List of PCI Equipments: \n" + remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
+        result = "\n List of PCI Equipments: \n" + remote_command_library.connecToServerExecuteCommand(self, hostname,
+                                                                                                       port, username,
+                                                                                                       password,
+                                                                                                       command)
         return result2 + result
 
-    #Smbios type16
-    def type16_smbios2(self, hostname, port, username, password):
+    #Get the Smbios type16
+    def type16_smbios2(self, hostname, port, username, password) -> str:
         # type 16
         command = r"wmic memorychip get BankLabel, Capacity, DeviceLocator, MemoryType, TypeDetail, FormFactor, DataWidth, Status"
         #command =r"wmic memorychip list full"
         result = remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
         return result
 
- #Smbios type17
-    def type17_smbios2(self, hostname, port, username, password):
+    #Get the Smbios type17
+    def type17_smbios2(self, hostname, port, username, password) -> str:
         # type 17
         command = r"wmic memorychip get BankLabel, Capacity, DeviceLocator, Manufacturer, PartNumber, Speed, SerialNumber"
         result = remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
         return result
-
-
-    # Reboot a macbook MacOS
-    def reboot_server_remotely(self, hostname, port, username, password):
-        command = r"echo 12345 | sudo -S reboot"
-        remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
-
-    #Return the Network Interface Information on MacOS
-    def get_network_info(self, hostname, port, username, password):
-        command = r"ifconfig"
-        result = remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
-        return result
-
-    def get_pcie_info(self, hostname, port, username, password):
-        #command = r"system_profiler SPPCIDataType"
-        command = "system_profiler SPThunderboltDataType SPUSBDataType SPNVMeDataType SPSerialATADataType SPStorageDataType SPNetworkDataType SPEthernetDataType SPAirPortDataType SPBluetoothDataType"
-        #command = "ioreg -l | grep \"Graphics\""
-        #command = "ioreg -l -S -c \"Capability\""
-        #command = "system_profiler | grep Memory"
-        #command = "system_profiler -listDataTypes"
-        #command = "system_profiler SPAudioDataType SPEthernetDataType SPDisplaysDataType SPMemoryDataType SPNetworkDataType"
-        result = remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
-        return result
-
-    def get_ram_info(self, hostname, port, username, password):
-        command = "system_profiler SPHardwareDataType | grep Memory"
-        #command = "system_profiler SPHardwareDataType"
-        result = remote_command_library.connecToServerExecuteCommand(self, hostname, port, username, password, command)
-        return result
-
